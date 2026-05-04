10a375eb451a94726f61adf98abe4fbf35c921d0
bigwiginfohub-wq <bigwiginfohub@gmail.com>
2026-05-04 15:56:06 +0530

    Update api.py

--- api.py
+++ api.py
@@ -130,7 +130,15 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc"
 )
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
